5cf4effd1d91b2b191b059e77700cdd1930c218a
ElmerPC <erey2790@gmail.com>
2022-04-14 23:45:37 -0500

    updating

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,10 +3,9 @@ import React from "react";
 
 function Nav() {
   const  categories = [
-    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+    { name: 'Home', description: 'Brings you back to homepage' },
+    { name: 'projects', description: 'List of projects' },
+    
   ];
 
   const handleClick = () => {
@@ -24,7 +23,7 @@ function Nav() {
         <ul className="flex-row">
           <li className="mx-2">
             <a href="#about" onClick={() => handleClick()}>
-              About me
+              About
             </a>
           </li>
           <li className={"mx-2"}>
