1265c301512234bf63ab445934d3e48aadcbe83c
ElmerPC <erey2790@gmail.com>
2022-04-20 01:52:20 -0500

    imported photos

--- src/components/AppList/index.js
+++ src/components/AppList/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import photoDispatch from '../../assets/pics/PortfolioDispatch.png';
+import photoEats from '../../assets/pics/PortfolioEats.png';
+import photoOvercast from '../../assets/pics/PortfolioOvercast.png';
+import photoSpar from '../../assets/pics/portfolioSpar.png';
+import photoSticheese from '../../assets/pics/PortfolioSticheese.png';
+import photoTea from '../../assets/pics/PortfolioTea.png';
 
 function AppList() {
     return (
         <div>
-            
+            <img
+            src={photoDispatch}
+            alt="dispatch example"
+            />
         </div>
     )
 }
