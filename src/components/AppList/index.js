3f2b02fd91c65ecc290995fa2dee771893e5517e
ElmerPC <erey2790@gmail.com>
2022-04-28 21:14:51 -0500

    added project to portfolio

--- src/components/AppList/index.js
+++ src/components/AppList/index.js
@@ -1,23 +1,28 @@
 // import React, { useState } from 'react';
-import photoDispatch from '../../assets/pics/PortfolioDispatch.png';
+// import photoDispatch from '../../assets/pics/PortfolioDispatch.png';
 import photoEats from '../../assets/pics/PortfolioEats.png';
 import photoOvercast from '../../assets/pics/PortfolioOvercast.png';
 import photoSpar from '../../assets/pics/PortfolioSpar.png';
 import photoSticheese from '../../assets/pics/PortfolioSticheese.png';
 // import photoTea from '../../assets/pics/PortfolioTea.png';
+import project3 from '../../assets/pics/project 3.png'
 
 function AppList() {
     
     return (
+        
         <div className='appPic'>
+            <a href='https://viewsnvibes.herokuapp.com/'></a>
             <img className='dispatch'
-            src={photoDispatch}
+            src={project3}
             alt="dispatch example"
+            
             />
 
             <img className='eats'
             src={photoEats}
             alt='eats example'
+            
             />
 
             <img className='overcast'
